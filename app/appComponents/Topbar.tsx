ce0d767f3a6e8590cd116af617da6b0e9d27fa61
ByteBrewer101 <161151563+ByteBrewer101@users.noreply.github.com>
2024-10-22 22:30:07 +0530

    topbar

--- app/appComponents/Topbar.tsx
+++ app/appComponents/Topbar.tsx
@@ -1,19 +1,22 @@
 import { Button } from "@/components/ui/button";
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
 import ProfileButton from "./Profilebtn";
+import { ModeToggle } from "./Darkmodebtn";
 
-
-export function Topbar(){
-    return (
-      <div className="flex justify-around p-4">
-        <div>
-          <h1>SiteName</h1>
-        </div>
-
-        <div className="flex justify-between flex-wrap-0 space-x-4">
-        
-<ProfileButton/>
-        </div>
+export function Topbar() {
+  return (
+    <div className="flex justify-around p-4 dark:bg-zinc-950 shadow-xl  ">
+      <div>
+        <h1 className="text-2xl dark:text-white text-slate-950 font-extrabold">
+          SiteName
+        </h1>
       </div>
-    );
+
+      <div className="flex justify-between flex-wrap-0 space-x-4">
+        <ModeToggle />
+
+        <ProfileButton />
+      </div>
+    </div>
+  );
 }
