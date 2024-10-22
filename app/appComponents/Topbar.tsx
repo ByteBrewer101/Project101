af1768c5c550e7c777315b7e81df477fe941860e
ByteBrewer101 <161151563+ByteBrewer101@users.noreply.github.com>
2024-10-22 23:03:24 +0530

    commit

--- app/appComponents/Topbar.tsx
+++ app/appComponents/Topbar.tsx
@@ -1,16 +1,24 @@
+"use client"
+
 import { Button } from "@/components/ui/button";
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
 import ProfileButton from "./Profilebtn";
-import { ModeToggle } from "./Darkmodebtn";
+import { ModeToggle } from "../../components/ui/ModeToggle";
 
 export function Topbar() {
-  return (
-    <div className="flex justify-around p-4 dark:bg-zinc-950 shadow-xl  ">
-      <div>
-        <h1 className="text-2xl dark:text-white text-slate-950 font-extrabold">
+  
+
+
+    return (
+
+    
+    <div className="flex justify-around p-4   ">
+      <div className="flex items-center justify-center">
+        <h1 className="text-xl dark:text-white text-slate-950 font-semibold">
           SiteName
         </h1>
       </div>
+
 
       <div className="flex justify-between flex-wrap-0 space-x-4">
         <ModeToggle />
--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,8 @@
-import Image from "next/image";
+
 import { Topbar } from "./appComponents/Topbar";
 
 export default function Home() {
-  return <div className="min-h-screen dark:bg-black">
+  return <div className=" dark:bg-black">
     <Topbar/>
   </div>
 }
